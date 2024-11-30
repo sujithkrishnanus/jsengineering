1cdcfade0673bf99dbfd53bc0639beefd8f2bdf8
sujithkrishnanus <sujithkrishnanus@gmail.com>
2024-11-30 11:03:30 +0530

    jscommit1

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn jsproject.wsgi --log-file -
--- jsproject/settings.py
+++ jsproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-evsvn#hnk3+9cqb7h%#u$h80y*=$iu*&s3be_^yz0t6=$hm8#+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'jsproject.urls'
